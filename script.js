abf4c50caf74e550e6a409dc02f2f21005f75cde
Seyon Rajagopal <r_seyon@live.ca>
2020-09-14 22:51:23 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+// 0,1 or space
+const getRandomNum = () => Math.random() < 0.4 ? '0' : Math.random() < 0.66 ? '1' : ' ';
+
+createBgNumbers();
+setInterval(changeNumbers, 2000);
+
+// create a random grid of 0,1, or space for background
+function createBgNumbers() {
+	const size = 30;
+	const row = Math.ceil(window.innerWidth / size);
+	const col = Math.ceil(window.innerHeight / size);
+	
+	for(let i=0; i<row; i++){
+		for(let j=0; j<col; j++){
+			const num = document.createElement('div');
+			num.classList.add('num');
+			num.style.left = `${i * size}px`;
+			num.style.top = `${j * size}px`;
+			num.innerHTML =`<span>${getRandomNum()}</span>`;
+			num.style.color = Math.random() < 0.2 ? '#fff' : '#2f3542';
+			document.body.appendChild(num);
+		}
+	}
+}
+
+function changeNumbers() {
+	const nums = document.querySelectorAll('.num');
+	
+	nums.forEach(num => {
+		num.innerHTML =`<span>${getRandomNum()}</span>`;
+		num.style.color = Math.random() < 0.2 ? '#fff' : '#2f3542';
+	});
+}
